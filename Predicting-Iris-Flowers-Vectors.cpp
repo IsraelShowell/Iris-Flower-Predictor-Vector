a30111c0b5799ee2b8458f5d5b4bb7b508aaa6d9
IsraelShowell <135654623+IsraelShowell@users.noreply.github.com>
2024-04-17 10:37:09 -0400

    Version 1.20

--- Predicting-Iris-Flowers-Vectors.cpp
+++ Predicting-Iris-Flowers-Vectors.cpp
@@ -5,7 +5,7 @@ End Date: 4-9-2024
 Project:
 Predicting Iris Flowers using Vectors
 
-Version: 1.1.0
+Version: 1.2.0
 
 
 Description:
@@ -22,13 +22,14 @@ This is the a newer version of the first code I made. This one uses vectors to i
 
 
 //Libraries used to provide functionalilty for the program
-#include <iostream>
-#include <fstream>
-#include <cstring>
-#include <string>
-#include <math.h>
-#include <cmath>
-#include <vector>
+#include <iostream> //Cout, Cin
+#include <fstream> //File Streaming
+#include <cstring> //Cstrings Management
+#include <string> //String Management
+#include <math.h> //Additional Mathematical Functions
+#include <cmath> //Additional Mathematical Functions
+#include <vector> //Used for better memory management
+#include <iomanip>  //Used to format output
 
 //My custom header file
 #include "Prediction.h"
@@ -146,6 +147,7 @@ int main()
 
         //Runs the likeihood and confidence functions
         Flower1.likeihood(density_setosa, density_ver,density_vir,numberoftype_Set, numberoftype_Ver,numberoftype_Vir, records);
+       
         Flower1.confidence(density_setosa, density_ver, density_vir);
 
 
@@ -159,9 +161,12 @@ int main()
         if (results[0] >= results[1] && results[0] >= results[2]) {
             cout << "The program has generated these results: " << endl;
 
-            cout << results[0] << " is the how confident the program is that your entered flower is a Setosa." << endl;
-            cout << results[1] << " is the how confident the program is that your entered flower is a Versicolor." << endl;
-            cout << results[2] << " is the how confident the program is that your entered flower is a Virginica." << endl;
+            //Formats the confidence as a percentage for better readability
+            cout << fixed << setprecision(3) << results[0] * 100 << '%' << " is how confident the program is that your entered flower is a Setosa." << endl;
+            cout << fixed << setprecision(3) << results[1] * 100 << '%' << " is how confident the program is that your entered flower is a Versicolor." << endl;
+            cout << fixed << setprecision(3) << results[2] * 100 << '%' << " is how confident the program is that your entered flower is a Virginica." << endl;
+           
+            //Saves the predicted flower type with the user's inputted attribute values
             userarr[4] = "Iris-setosa";
 
             cout << "Thus, your flower is most likely a Setosa!" << endl;
@@ -169,28 +174,32 @@ int main()
         else if (results[1] >= results[0] && results[1] >= results[2]) {
             cout << "The program has generated these results: " << endl;
 
-            cout << results[0] << " is the how confident the program is that your entered flower is a Setosa." << endl;
-            cout << results[1] << " is the how confident the program is that your entered flower is a Versicolor." << endl;
-            cout << results[2] << " is the how confident the program is that your entered flower is a Virginica." << endl;
+            //Formats the confidence as a percentage for better readability
+            cout << fixed << setprecision(3) << results[0] * 100 << '%' << " is how confident the program is that your entered flower is a Setosa." << endl;
+            cout << fixed << setprecision(3) << results[1] * 100 << '%' << " is how confident the program is that your entered flower is a Versicolor." << endl;
+            cout << fixed << setprecision(3) << results[2] * 100 << '%' << " is how confident the program is that your entered flower is a Virginica." << endl;
+           
+            //Saves the predicted flower type with the user's inputted attribute values
             userarr[4] = "Iris-versicolor";
 
             cout << "Thus, your flower is most likely a Versicolor!" << endl;
         }
         else {
             cout << "The program has generated these results: " << endl;
-
-            cout << results[0] << " is the how confident the program is that your entered flower is a Setosa." << endl;
-            cout << results[1] << " is the how confident the program is that your entered flower is a Versicolor." << endl;
-            cout << results[2] << " is the how confident the program is that your entered flower is a Virginica." << endl;
+            
+            //Formats the confidence as a percentage for better readability
+            cout << fixed << setprecision(3) << results[0] * 100 << '%' << " is how confident the program is that your entered flower is a Setosa." << endl;
+            cout << fixed << setprecision(3) << results[1] * 100 << '%' << " is how confident the program is that your entered flower is a Versicolor." << endl;
+            cout << fixed << setprecision(3) << results[2] * 100 << '%' << " is how confident the program is that your entered flower is a Virginica." << endl;
+            
+            //Saves the predicted flower type with the user's inputted attribute values
             userarr[4] = "Iris-virginica";
 
             cout << "Thus, your flower is most likely a Virginica!" << endl;
         }
-        
 
-        //cout << "Would you like to add your new flower to your dataset?" << endl;
-        
-        cout << "Please enter 1 to add the flower, or anything else to continue!" << endl;
+        cout << endl;
+        cout << "Please enter 1 to add the flower as a " << userarr[4] <<" to the CSV file, or anything else to continue!" << endl;
         cout << "Please note; Once a flower has been added, the next flower you enter will be influnced by the new entry!" << endl;
 
         int choice2 = 0;
@@ -199,24 +208,24 @@ int main()
 
         cin.ignore();
 
-        cout << userarr[4] << endl;
-
+        //This controls the functions of adding the flower to the database
         if (choice2 == 1) {
             
             Flower1.senddata(userarr,records);
             cout << "Flower was successfully added to the dataset!" << endl;
+
+            //This was used to support adding multiple flowers in one session, however I removed that functionality for now
             datasetinfo.erase(datasetinfo.begin() + 1, datasetinfo.end());
             datasetinfo[0].clear();
         }
         else {
             cout << "Flower was NOT added to the dataset!" << endl;
+
+            //This was used to support adding multiple flowers in one session, however I removed that functionality for now
             datasetinfo.erase(datasetinfo.begin() + 1,datasetinfo.end());
             datasetinfo[0].clear();
         }
-
-        //goto MENU;
     }
-//} while (choice == 1);
     cout << "Program End" << endl;
 system("PAUSE");
 }
